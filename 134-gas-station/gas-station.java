7d6c728a8a679b7cd7c8440404b6d9dd735d8e67
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-09-12 10:01:56 -0400

    Time: 2 ms (97.02%) | Memory: 56.8 MB (8.53%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -3,22 +3,14 @@ class Solution {
         
         int allRemainGas = 0; // all need gas
         int hasGas = 0; // last time has gas
-        int start = -1;
+        int start = 0;
         for(int i = 0;i<gas.length;i++){
-            if(start == -1 && gas[i] >= cost[i]){
-                // first time
-                start = i;
-                hasGas = gas[i] - cost[i];
-                allRemainGas +=gas[i] - cost[i];
-            }else {
-                hasGas += gas[i] - cost[i];
-                allRemainGas +=gas[i] - cost[i];
-                if(hasGas < 0){
-                    hasGas = 0;
-                    start = -1;
-                }
+            hasGas += gas[i] - cost[i];
+            allRemainGas +=gas[i] - cost[i];
+            if(hasGas < 0){
+                hasGas = 0;
+                start = i+1;
             }
-            
         }
         return allRemainGas >= 0 ? start : -1; 
 
