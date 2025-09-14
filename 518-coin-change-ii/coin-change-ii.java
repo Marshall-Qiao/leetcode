3320bdec06d249dcc7507aeee4c6a7bb2e55060d
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-09-13 23:32:08 -0400

    Time: 3 ms (99.77%) | Memory: 41.3 MB (82.38%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,15 +1,25 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-        Integer[][] memo = new Integer[amount + 1][coins.length];
-        return dp(amount, coins, 0,memo);
+     public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int one : coins){
+            for(int i = one; i<= amount;i++){
+                dp[i] += dp[i-one]; 
+            }
+        }
+        return dp[amount];
     }
+    // public int change(int amount, int[] coins) {
+    //     Integer[][] memo = new Integer[amount + 1][coins.length];
+    //     return dp(amount, coins, 0,memo);
+    // }
 
-    public int dp(int amount, int[] coins, int index,Integer[][] memo){
-        if(amount == 0) return 1;
-        if(amount < 0 || index >= coins.length) return 0;
-        if(memo[amount][index] != null) return memo[amount][index];
-        int res =  dp(amount - coins[index], coins, index,memo)+ dp(amount, coins, index+1,memo);
-        memo[amount][index] = res;
-        return res;
-    }
+    // public int dp(int amount, int[] coins, int index,Integer[][] memo){
+    //     if(amount == 0) return 1;
+    //     if(amount < 0 || index >= coins.length) return 0;
+    //     if(memo[amount][index] != null) return memo[amount][index];
+    //     int res =  dp(amount - coins[index], coins, index,memo)+ dp(amount, coins, index+1,memo);
+    //     memo[amount][index] = res;
+    //     return res;
+    // }
 }
