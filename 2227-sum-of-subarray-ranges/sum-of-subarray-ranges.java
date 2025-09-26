c8b64050f40ef9c4558ec236f0a94154a4b38cfb
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-09-26 14:54:21 -0400

    Time: 18 ms (63.15%) | Memory: 44.3 MB (82.57%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,14 +1,39 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long count = 0;
-        for(int i = 0; i< nums.length;i++){
-            int min = nums[i], max = nums[i];
-            for(int j = i; j< nums.length;j++){
-                min=Math.min(min,nums[j]);
-                max=Math.max(max,nums[j]);
-                count+= max-min;
+        long res = 0;
+        Stack<Integer> stack = new Stack<>();
+        // find the sum of all minimum 
+        for(int i = 0;i <= nums.length;i++){
+            while(!stack.isEmpty() && (i == nums.length || nums[stack.peek()] >= nums[i])){
+                int mid = stack.peek();
+                stack.pop();
+                int left = stack.isEmpty()? -1:stack.peek();
+                res -=(long) nums[mid]*(i-mid)*(mid-left);
             }
+            stack.add(i);
         }
-        return count;
+        stack.clear();
+        for(int i = 0;i <= nums.length;i++){
+             while(!stack.isEmpty() && (i == nums.length || nums[stack.peek()] <= nums[i])){
+                int mid = stack.peek();
+                stack.pop();
+                int left = stack.isEmpty()? -1:stack.peek();
+                res +=(long) nums[mid]*(i-mid)*(mid-left);
+            }
+            stack.add(i);
+        }
+        return res;
     }
+    // public long subArrayRanges(int[] nums) {
+    //     long count = 0;
+    //     for(int i = 0; i< nums.length;i++){
+    //         int min = nums[i], max = nums[i];
+    //         for(int j = i; j< nums.length;j++){
+    //             min=Math.min(min,nums[j]);
+    //             max=Math.max(max,nums[j]);
+    //             count+= max-min;
+    //         }
+    //     }
+    //     return count;
+    // }
 }
