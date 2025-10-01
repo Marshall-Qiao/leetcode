99ad0531065924faa5fed3ccab417af61e4842aa
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-10-01 09:58:35 -0400

    Time: 1 ms (81.25%) | Memory: 40.9 MB (72.91%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+       long res = 0;
+       while(x != 0){
+         int key = x % 10;
+         res = res * 10 + key;
+         x = x / 10;
+       }
+       if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE ) return 0;
+       return (int)res;
+    }
+}
