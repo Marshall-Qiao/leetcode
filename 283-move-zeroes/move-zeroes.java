6c27c7c14fd9b6695ae9caeaa25ed6361010f3a7
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-10-21 21:08:23 -0400

    Time: 2 ms (79.04%) | Memory: 46.4 MB (26.96%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,29 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                q.offer(i);
-                continue;
-            }
-            if (!q.isEmpty()) {
-                int zero = q.poll().intValue();
-                int temp = nums[zero];
-                nums[zero] = nums[i];
-                nums[i] = temp;
-                q.offer(i);
+        int left = 0;
+        for (int right = 0; right < nums.length; right++){
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
         }
     }
+    // public void moveZeroes(int[] nums) {
+    //     Queue<Integer> q = new LinkedList<>();
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (nums[i] == 0) {
+    //             q.offer(i);
+    //             continue;
+    //         }
+    //         if (!q.isEmpty()) {
+    //             int zero = q.poll().intValue();
+    //             int temp = nums[zero];
+    //             nums[zero] = nums[i];
+    //             nums[i] = temp;
+    //             q.offer(i);
+    //         }
+    //     }
+    // }
 }
