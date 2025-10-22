3b8db1e7565d9f6c507193918f9682d5cf61ba98
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-10-22 19:01:20 -0400

    Time: 14 ms (26.20%) | Memory: 45.5 MB (58.04%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -3,20 +3,19 @@ class Solution {
         Set<Integer> set1 = Arrays.stream(nums1)
                                      .boxed() // Convert int to Integer objects
                                      .collect(Collectors.toCollection(HashSet::new));
+
         Set<Integer> set2 = Arrays.stream(nums2)
                                      .boxed() // Convert int to Integer objects
                                      .collect(Collectors.toCollection(HashSet::new));
         List<List<Integer>> res = new ArrayList<>();
-        Set<Integer> list1 = new HashSet<>(); 
-        Set<Integer> list2 = new HashSet<>(); 
-        for (int one : nums1) {
+        List<Integer> list1 = new ArrayList<>(); res.add(list1);
+        List<Integer> list2 = new ArrayList<>(); res.add(list2);
+        for (int one : set1) {
             if (!set2.contains(one)) list1.add(one);
         }
-         for (int one : nums2) {
+         for (int one : set2) {
             if (!set1.contains(one)) list2.add(one);
         }
-        res.add(new ArrayList<>(list1));
-        res.add(new ArrayList<>(list2));
         return res;
     }
 }
