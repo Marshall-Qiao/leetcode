beb3b9b421419bc26a5be2150d0759a461c3254d
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-10-27 19:51:47 -0400

    Time: 8 ms (49.57%) | Memory: 42.8 MB (19.23%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,27 +1,43 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        Map<Character,int[]> map = new HashMap<>();
+        Map<Character,Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            int[] nowInt = map.getOrDefault(s.charAt(i),new int[]{i,i});
-            nowInt[1] = i;
-            map.put(s.charAt(i), nowInt);
+            map.put(s.charAt(i), i);
         }
         List<Integer> list = new ArrayList<>();
-        int l = 0;
-        while (l < s.length()) {
-            int[] nowInt = map.get(s.charAt(l));
-            int thisLast = nowInt[1];
-            int right = l + 1;
-            while (right < thisLast) {
-                int end = thisLast;
-                for (; right < end; right++) {
-                    nowInt = map.get(s.charAt(right));
-                    thisLast = Math.max(thisLast, nowInt[1]);
-                }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, map.get(s.charAt(i)));
+            if (i == end) {
+                list.add(end - start + 1);
+                start = i + 1;   
             }
-            list.add(thisLast - l + 1);
-            l = thisLast + 1;            
         }
         return list;
     }
+    // public List<Integer> partitionLabels(String s) {
+    //     Map<Character,int[]> map = new HashMap<>();
+    //     for (int i = 0; i < s.length(); i++) {
+    //         int[] nowInt = map.getOrDefault(s.charAt(i),new int[]{i,i});
+    //         nowInt[1] = i;
+    //         map.put(s.charAt(i), nowInt);
+    //     }
+    //     List<Integer> list = new ArrayList<>();
+    //     int l = 0;
+    //     while (l < s.length()) {
+    //         int[] nowInt = map.get(s.charAt(l));
+    //         int thisLast = nowInt[1];
+    //         int right = l + 1;
+    //         while (right < thisLast) {
+    //             int end = thisLast;
+    //             for (; right < end; right++) {
+    //                 nowInt = map.get(s.charAt(right));
+    //                 thisLast = Math.max(thisLast, nowInt[1]);
+    //             }
+    //         }
+    //         list.add(thisLast - l + 1);
+    //         l = thisLast + 1;            
+    //     }
+    //     return list;
+    // }
 }
