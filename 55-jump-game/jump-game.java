6354d047e8157c065c6f8fc253b5ffa267e68be2
Marshall-Qiao <qiaoyuanshou@gmail.com>
2025-11-27 17:10:58 -0500

    Time: 77 ms (12.44%) | Memory: 48 MB (6.73%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,22 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int goal = nums.length - 1;
-        for(int i = nums.length - 2 ; i >= 0; i--){
-            if(i+nums[i] >= goal)goal = i;
+        boolean[] dp = new boolean[nums.length];
+        dp[0] = true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (!dp[i]){
+                return false;
+            }
+            if (i + nums[i] >= nums.length - 1) return true;
+            for (int m = 0; m < nums[i]; m++){
+                dp[i + m + 1] = true;
+            }
         }
-        return goal == 0;
+        return dp[nums.length - 1];
     }
-    // public boolean canJump(int[] nums) {
-    //    if(nums == null || nums.length == 0) return true;
-    //    boolean[] visited = new boolean[nums.length];
-    //    visited[0] = true; 
-    //    for(int i = 0; i< nums.length;i++){
-    //       if(!visited[i]) return false;
-    //       if(i+nums[i] >= nums.length-1) return true;
-    //       for(int j=i+1;j<= i+nums[i];j++){
-    //          visited[j] = true;
-    //       }
-    //    }
-    //    return true;
-    // }
 }
