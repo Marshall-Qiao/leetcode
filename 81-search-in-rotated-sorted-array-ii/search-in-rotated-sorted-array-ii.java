7b994c1820cf3e66a5a22d20ee27bd6043250451
Marshall-Qiao <qiaoyuanshou@gmail.com>
2026-03-01 08:16:53 -0500

    Time: 0 ms (100.00%) | Memory: 44.8 MB (96.30%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (target == nums[mid] || target == nums[l]) return true;
-            while (l <= r && nums[l] == nums[r]){
-                r--;
-                l++;
-            } 
-            if (l > r) break;
-            if (nums[l] <= nums[mid]) {
-                if (target < nums[mid] && target >= nums[l]) {
-                    r = mid - 1;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[left] == nums[mid]) {
+                left++;
+                continue;
+            }
+            if (nums[left] < nums[mid]) {
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
                 } else {
-                    l = mid + 1;
+                    left = mid + 1;
                 }
             } else {
-                if (target > nums[mid] && target <= nums[r]) {
-                    l = mid + 1;
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
                 } else {
-                    r = mid - 1; 
+                   right = mid - 1;
                 }
             }
         }
