50eec27f4da03114b048aa0aefafea46e72782cd
Marshall-Qiao <qiaoyuanshou@gmail.com>
2026-03-02 20:05:58 -0500

    Time: 1 ms (99.75%) | Memory: 77.1 MB (83.61%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,15 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       int count = 0,best = Integer.MIN_VALUE;
-       for(int i = 0; i< nums.length;i++){
-            count += nums[i];
-            best = Math.max(best,count);
-            if(count < 0){ 
-                count = nums[i] < 0 ? 0 : nums[i];
-                continue;
-            } 
+       int count = 0, best = Integer.MIN_VALUE;
+       for (int num : nums) {
+         count += num;
+         best = Math.max(best, count);
+         if (count < 0) {
+            count = 0;
+         }
        }
        return best;
-        
     }
 }
