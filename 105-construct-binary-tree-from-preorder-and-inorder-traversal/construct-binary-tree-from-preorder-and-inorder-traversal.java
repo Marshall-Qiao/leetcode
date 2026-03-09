06b3256291731a3d251357225dc2c815abeac0fe
Marshall-Qiao <qiaoyuanshou@gmail.com>
2026-03-09 17:56:27 -0400

    Time: 2 ms (64.94%) | Memory: 46.2 MB (73.64%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -29,13 +29,9 @@ class Solution {
         int tempIndex = inorderMap.get(preorder[preLeft]);
         int leftCount = tempIndex - inLeft;
         int rightCount = inRight - tempIndex;
-        if (leftCount >= 1) {
             newTemp.left = buildTreeByIndex(preorder, preLeft + 1, preLeft + leftCount, inorder, inLeft, tempIndex - 1, inorderMap);
-        }
 
-        if (rightCount >= 1) {
             newTemp.right = buildTreeByIndex(preorder, preRight - rightCount + 1, preRight, inorder, tempIndex + 1, inRight, inorderMap);
-        }
         return newTemp;
     }
 
