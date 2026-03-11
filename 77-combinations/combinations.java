acd688a85af8cc55ce16e72f028245fb0b73e624
Marshall-Qiao <qiaoyuanshou@gmail.com>
2026-03-11 14:59:24 -0400

    Time: 25 ms (13.41%) | Memory: 98.7 MB (6.14%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,26 +1,20 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        if(k>n) return new ArrayList<>();
-        List<List<Integer>> res = new LinkedList<List<Integer>>();
-        List<Integer> one = new LinkedList<Integer>();
-        combinationFromN(res,one,n,k,1);
-        return res;
+       List<List<Integer>> result = new ArrayList<>();
+       backTrack(n, k, 0, new LinkedList<Integer>(),result);
+       return result;  
     }
 
-    public void combinationFromN(List<List<Integer>> res,List<Integer> one,int n, int k,int from) {
-        if(k == 1){
-            for(int i = from; i <= n; i++){
-                one.add(i);
-                res.add(new LinkedList<>(one));
-                one.removeLast();
-            }
+    public void backTrack(int n, int k, int index, List<Integer> list, List<List<Integer>> result) {
+        if (n < k) return;
+        if (list.size() == k) {
+            result.add(new ArrayList<>(list));
             return;
         }
-       for(int i = from; i <= n; i++){
-          one.add(i);
-          combinationFromN(res,one,n,k-1,i+1);
-          one.removeLast();
-       }
+        for (int i = index; i < n; i++) {
+            list.add(i+1);
+            backTrack(n, k, i + 1, list,result);
+            list.removeLast();
+        }
     }
-
 }
