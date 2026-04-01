948730c1d5d08eb5a1f3c3ebac7062f099d15243
Marshall-Qiao <qiaoyuanshou@gmail.com>
2026-04-01 16:00:12 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (42.87%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,16 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l, idx, r = 0, 0, len(nums) - 1
-        while idx <= r:
-            if nums[idx] == 0: 
-                nums[idx], nums[l] =  nums[l], nums[idx]
-                l += 1
+        # l, idx, r = 0, 0, len(nums) - 1
+        # while idx <= r:
+        #     if nums[idx] == 0: 
+        #         nums[idx], nums[l] =  nums[l], nums[idx]
+        #         l += 1
+        #         idx += 1
+        #     elif nums[idx] == 2:
+        #         nums[idx], nums[r] =  nums[r], nums[idx]
+        #         r -= 1
+        #     else:
+        #         idx += 1            
+        map = {}
+        for num in nums:
+            map[num] = map.get(num, 0) + 1
+
+        idx = 0
+        for clor in range(3):
+            for _ in range(map.get(clor, 0)):
+                nums[idx] = clor
                 idx += 1
-            elif nums[idx] == 2:
-                nums[idx], nums[r] =  nums[r], nums[idx]
-                r -= 1
-            else:
-                idx += 1            
-    
+
         
