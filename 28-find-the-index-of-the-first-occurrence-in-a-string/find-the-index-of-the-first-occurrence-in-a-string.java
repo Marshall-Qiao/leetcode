8415311b4d0a6bb0f2a10954e5009ad3224928d8
Marshall <qiaoyuanshou@gmail.com>
2026-09-17 11:34:57 -0400

    Time: 1 ms (49.87%) | Memory: 42.6 MB (95.94%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,41 +1,19 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int peer = 0; int min = Integer.MAX_VALUE;
-        List<Integer> misList = new ArrayList<Integer>();
-
-        for(int i = 0; i< haystack.length();i++){
-            char hay = haystack.charAt(i);
-            if(hay != needle.charAt(peer) && hay != needle.charAt(0)){
-                peer = 0;
-                continue;
-            }
-            if(hay != needle.charAt(peer)){
-               peer = 0; 
-            }
-
-            peer++;
-            if(peer == needle.length()){
-                min = Math.min(min,i-peer+1);
-                peer = 0;
-                continue;
-            }
-
-            if(peer != 1 && haystack.charAt(i) == needle.charAt(0)){
-                misList.add(i);
-            }
-        }
-
-        for(int i = 0; i< misList.size();i++){
-            Integer start = misList.get(i);
-            boolean isMatch = true;
-            for(int j = start; j< (start+needle.length());j++ ){
-                if(j>=haystack.length() || haystack.charAt(j) != needle.charAt(j-start)){
-                    isMatch = false;
-                    break;
+        if (haystack == null || needle == null || haystack.length() < needle.length()) return -1;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                if (needle.length() == 1) return i;
+                boolean isSame = true;
+                for (int n = 1; n < needle.length(); n++) {
+                    if (haystack.charAt(i + n) != needle.charAt(n)) {
+                        isSame = false;
+                        break;
+                    }
                 }
+                if (isSame) return i;
             }
-            if(isMatch) min = Math.min(min,start);
         }
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return -1;
     }
 }
